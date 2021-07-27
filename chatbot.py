1be1fa4197419517206e9990f3613d64f06096a0
Lotfi <hl_feraga@esi.dz>
2021-07-27 17:25:56 +0100

    importing

--- chatbot.py
+++ chatbot.py
@@ -1,1 +1,12 @@
 #creating a GUI to make it smooth and user friendly
+import nltk
+from nltk.stem import WordNetLemmatizer
+import pickle
+import numpy as np 
+from keras.models import load_model
+model = load_model('chatbot_model.h5')
+import json
+import random
+intents =json.loads(open('intents.json').read())
+words = pickle.load(open('words.pkl','rb'))
+classes = pickle.load(open('classes.pkl','rb'))
