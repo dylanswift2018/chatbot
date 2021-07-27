63f3172df24e22406a4a625f3ee72956d934d9ba
Lotfi <hl_feraga@esi.dz>
2021-07-27 17:52:41 +0100

    almost

--- chatbot.py
+++ chatbot.py
@@ -43,4 +43,17 @@ def predict_class(sen , model) :
     for x in res :
         ret_list.append({"intent" : classes[x[0]],"probabilty": str(x[1])})
     return ret_list
+
+def getResponse(ints, intents_json):
+    tag = ints[0]['intent']
+    list_of_intents = intents_json['intents']
+    for i in list_of_intents:
+        if(i['tag']== tag):
+            result = random.choice(i['responses'])
+            break
+    return result
     
+def chatbot_response(text):
+    ints = predict_class(text, model)
+    res = getResponse(ints, intents)
+    return res
