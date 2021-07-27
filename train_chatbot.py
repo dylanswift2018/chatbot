39fe7da30e27f51be01d33eacac0901c68e4cf10
Lotfi <hl_feraga@esi.dz>
2021-07-27 17:17:13 +0100

    adding the model

--- train_chatbot.py
+++ train_chatbot.py
@@ -88,8 +88,18 @@ print("Training data created")
 #we gonna build a DNN that has 3 layers usin keras sequential API
 
 model = Sequential()
-model.add(Dense(128 , inpu_shape = (len(train_x[0]),),activation= 'relu'))
+model.add(Dense(128 , input_shape = (len(train_x[0]),),activation= 'relu'))
 model.add(Dropout(0.5))
 model.add(Dense(64,activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(len(train_y[0]), activation ='softmax'))
+
+#compiling the model 
+sgd = SGD(lr =0.01, decay=1e-6, momentum=0.9,nesterov = True)
+model.compile(loss='categorical_crossentropy',optimizer=sgd,metrics=['accuracy'])
+
+#fitting and saving the model 
+hist = model.fit(np.array(train_x),np.array(train_y),epochs=200, batch_size=5,verbose=1)
+model.save('chatbot_model.h5',hist)
+
+print("model created :) ")
