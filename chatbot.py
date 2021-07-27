61f9f25665fefc6eadd2de6332121989a30696e1
Lotfi <hl_feraga@esi.dz>
2021-07-27 17:37:27 +0100

    GUI

--- chatbot.py
+++ chatbot.py
@@ -10,3 +10,25 @@ import random
 intents =json.loads(open('intents.json').read())
 words = pickle.load(open('words.pkl','rb'))
 classes = pickle.load(open('classes.pkl','rb'))
+
+#functions that perform preprocessing and predicting the text 
+def clean_up_sentence(sent) :
+    #split words into array 
+    sw = nltk.word_tokenize(sent)
+    sw = [WordNetLemmatizer.lemmatize(word.lower()) for word in sw]
+    return sw
+
+def bow(sen , words, details = True) :
+    #tokenize the pattern 
+    sw = clean_up_sentence(sen)
+    #matrix of words 
+    bag = [0] *len(words)
+    for x in sw :
+        for y,z in enumerate(words):
+            if z==x :
+                bag[y]=1
+                if details :
+                    print("found in bag : %s" % z)
+    return (np.array(bag))
+    
+     
